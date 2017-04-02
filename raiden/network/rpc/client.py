496f442d6ae4570553589917749ed648490eafd9
Hrishikesh Shrikant Huilgolkar <hrishikesh911@gmail.com>
2017-04-03 00:30:37 +0530

    fix

--- raiden/network/rpc/client.py
+++ raiden/network/rpc/client.py
@@ -1081,13 +1081,13 @@ class NettingChannel(object):
             )
 
     def settle(self):
-        estimated_gas = self.proxy.settle.estimate_gas(
-            startgas=self.startgas,
-            gasprice=self.gasprice,
-        )
+        # estimated_gas = self.proxy.settle.estimate_gas(
+        #     startgas=self.startgas,
+        #     gasprice=self.gasprice,
+        # )
 
         transaction_hash = self.proxy.settle.transact(
-            startgas=estimated_gas,
+            startgas=self.startgas,
             gasprice=self.gasprice,
         )
 
