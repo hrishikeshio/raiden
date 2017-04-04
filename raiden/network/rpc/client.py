6d96dae0cc14163ba98df1b77b9949e436141814
Hrishikesh Shrikant Huilgolkar <hrishikesh911@gmail.com>
2017-04-04 08:51:57 +0530

    filter changes debug2

--- raiden/network/rpc/client.py
+++ raiden/network/rpc/client.py
@@ -378,9 +378,9 @@ class Filter(object):
         # geth could return None
         if filter_changes is None:
             return []
-        if function=="eth_getFilterChanges":
-            log.debug ("filter_changes");
-            log.debug (filter_changes);
+        if function == "eth_getFilterChanges":
+            log.debug("filter_changes")
+            log.debug(filter_changes)
         result = list()
         for log_event in filter_changes:
             address = address_decoder(log_event['address'])
